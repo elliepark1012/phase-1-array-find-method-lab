474202edb735c97e5cfeaed068376f29feeb4735
Ellie <elliepark1012@gmail.com>
2022-07-10 09:19:45 -0400

    commit tree 6042b23640ba8243a27576fc33c0e30e92c4f2e6

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function superbowlWin(game){
+const wonYear = game.find(game => game.result === "W")
+return !! wonYear ? wonYear.year : undefined
+}
+
+console.log(superbowlWin(record))
